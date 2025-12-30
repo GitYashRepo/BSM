c7f38a8657810e700ad7e828063bcdf46d1926e9
yashrepogit <yas@cloudify.biz>
2025-12-30 11:56:22 +0530

    fixed page

---
+++ src/app/about/bsm/page.jsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function AboutBSMPage() {
+
+   return (
+      <div className="flex justify-center items-center min-h-screen bg-background">
+         About BSM
+      </div>
+   )
+}
