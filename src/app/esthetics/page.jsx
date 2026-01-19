f7e4d2cd65db603f990d9a67a957c4370dad665d
yashrepogit <yas@cloudify.biz>
2026-01-19 20:06:40 +0530

    added navbar links

---
+++ src/app/esthetics/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+
+import React from 'react';
+
+export default function EstheticsPage() {
+   return (
+      <div className="min-h-screen flex items-center justify-center">
+         <h1 className="text-4xl font-bold">Esthetics Page Coming Soon!</h1>
+      </div>
+   );
+}
---
+++ src/app/hair/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+
+import React from 'react';
+
+export default function HairPage() {
+   return (
+      <div className="min-h-screen flex items-center justify-center">
+         <h1 className="text-4xl font-bold">Hair Page Coming Soon!</h1>
+      </div>
+   );
+}
